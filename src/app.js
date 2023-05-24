eab256a4f04338ac2ecf96e1f268750118971b0e
LeandroAgustinFernandez <leandroa.fernandez@gmail.com>
2023-05-23 21:07:54 -0300

    LoginWithSessions V1.0.2

--- src/app.js
+++ src/app.js
@@ -1,34 +1,41 @@
 import cartRouter from "./routes/cart.router.js";
 import productsRouter from "./routes/products.router.js";
 import viewsRouter from "./routes/view.router.js";
-import sessionRouter from './routes/session.router.js'
+import sessionRouter from "./routes/session.router.js";
 import handlebars from "express-handlebars";
 import express from "express";
 import cors from "cors";
 import { __dirname, PORT, DB_USER, DB_PASS } from "./utils.js";
 import { Server } from "socket.io";
 import mongoose from "mongoose";
-import MongoStore from "connect-mongo"
+import MongoStore from "connect-mongo";
 import cookieParser from "cookie-parser";
 import session from "express-session";
 
 const app = express();
 
-app.use(session({
-  store: MongoStore.create({
-    mongoUrl:`mongodb+srv://${DB_USER}:${DB_PASS}@coderhousecluster.phsl88g.mongodb.net/ecommerce?retryWrites=true&w=majority`,
-    mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true }
-  }),
-  secret: "LeanBack",
-  resave: true,
-  saveUninitialized: true
-}))
+try {
+  mongoose.connect(
+    `mongodb+srv://${DB_USER}:${DB_PASS}@coderhousecluster.phsl88g.mongodb.net/ecommerce?retryWrites=true&w=majority`
+  );
+  console.log(`Base de datos conectada`);
+} catch (error) {
+  console.log(error.message);
+}
 
-mongoose.connect(
-  `mongodb+srv://${DB_USER}:${DB_PASS}@coderhousecluster.phsl88g.mongodb.net/ecommerce?retryWrites=true&w=majority`
+app.use(
+  session({
+    store: MongoStore.create({
+      client: mongoose.connection.getClient(),
+      mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
+    }),
+    secret: "LeanBack",
+    resave: true,
+    saveUninitialized: true,
+  })
 );
 
-app.use(cookieParser('LeanBack'))
+app.use(cookieParser("LeanBack"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
